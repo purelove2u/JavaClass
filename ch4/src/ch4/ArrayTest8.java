ec9c0bf2923e0aa3ba1ed8d099bbbeedb03cc897
purelove2u <59471889+purelove2u@users.noreply.github.com>
2021-08-18 18:38:52 +0900

    ssss

--- ch4/src/ch4/ArrayTest8.java
+++ ch4/src/ch4/ArrayTest8.java
@@ -3,20 +3,21 @@ package ch4;
 public class ArrayTest8 {
 
 	public static void main(String[] args) {
-		// 타입 배열명[][] = new 타입[2][2];
-		int arr1[][] = new int[2][2];
+		// íƒ€ìž… ë°°ì—´ëª…[][] = new íƒ€ìž…[2][2];
+		int arr1[][] = new int[3][5];
 		
-		arr1[0][0] = 15;
-		arr1[0][1] = 25;
-		arr1[1][0] = 35;
-		arr1[1][1] = 45;
+//		arr1[0][0] = 15;
+//		arr1[0][1] = 25;
+//		arr1[1][0] = 35;
+//		arr1[1][1] = 45;
 		
-		for(int i = 0 ; i < arr1.length ; i++) {			//행 길이
-			for(int j = 0 ; j < arr1[i].length ; j++){		//열 길이
-				System.out.printf("[%d][%d] = ", i, j);
-				System.out.println(arr1[i][j]);
+		for(int i = 0 ; i < arr1.length ; i++) {			//í–‰ ê¸¸ì�´
+			for(int j = 0 ; j < arr1[i].length ; j++){		//ì—´ ê¸¸ì�´
+//				System.out.printf("[%d][%d] = ", i, j);
+				arr1[i][j] = (j * 3) + (i + 1); 
+				System.out.print(arr1[i][j] + " ");
 			}
+			System.out.println();
 		}
-		System.out.println();
 	}
 }
